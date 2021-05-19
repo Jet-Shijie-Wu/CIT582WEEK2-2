54730578b3ef944641477f3fb676adf262282955
Jet-Shijie-Wu <70357730+Jet-Shijie-Wu@users.noreply.github.com>
2021-05-19 15:31:20 +0800

    Update hash_preimage.py

--- hash_preimage.py
+++ hash_preimage.py
@@ -6,5 +6,15 @@ def hash_preimage(target_string):
         print( "Input should be a string of bits" )
         return
     nonce = b'\x00'
+    
+    len_k = len(target_string)
+    while True:
+        x = ''.join(random.sample(string.ascii_letters, 30))
+        byte_x = x.encode('utf-8')
+        hex_x = bin(int(hashlib.sha256(byte_x).hexdigest(), 16))
+
+        if hex_x[-len_k:] == target_string:
+            return (byte_x)
+        continue
 
     return( nonce )
