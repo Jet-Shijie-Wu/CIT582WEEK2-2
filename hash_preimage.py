f3830032b6bffc0dc8fd7a9486ba9f451b894b82
Jet-Shijie-Wu <70357730+Jet-Shijie-Wu@users.noreply.github.com>
2021-05-22 07:08:03 +0800

    Update hash_preimage.py

--- hash_preimage.py
+++ hash_preimage.py
@@ -12,7 +12,7 @@ def hash_preimage(target_string):
     len_k = len(target_string)
     while True:
         x = ''.join(random.sample(string.ascii_letters, 30))
-        byte_x = os.urandom(30)
+        byte_x = x.encode('utf-8')
         hex_x = bin(int(hashlib.sha256(byte_x).hexdigest(), 16))
 
         if hex_x[-len_k:] == target_string:
